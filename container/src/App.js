fa0b2801e88a6a4c55a81f155cca2c57fa6e9b79
nikhil sharma <ns515991@gmail.com>
2024-12-06 01:44:57 +0530

    header implemented

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import MainMarketingApp from "./components/MarketingApp";
-export default ()=>{
-    return <div>
-       
-        <MainMarketingApp/>
-       
-    </div>
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom"
+export default () => {
+    return <BrowserRouter>
+        <div>
+            <Header />
+            <MainMarketingApp />
+        </div>
+    </BrowserRouter>
 }
