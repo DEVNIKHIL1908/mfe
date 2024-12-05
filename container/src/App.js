2e02c2a382c6e65497187289393c43c12cdc10b0
nikhil sharma <ns515991@gmail.com>
2024-12-06 02:27:47 +0530

    fix css bugs

--- container/src/App.js
+++ container/src/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import MainMarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom"
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles"
+const preClasas = createGenerateClassName({
+    productionPrefix: "co"
+})
 export default () => {
     return <BrowserRouter>
-        <div>
-            <Header />
-            <MainMarketingApp />
-        </div>
+        <StylesProvider generateClassName={preClasas}>
+            <div>
+                <Header />
+                <MainMarketingApp />
+            </div>
+        </StylesProvider>
     </BrowserRouter>
 }
