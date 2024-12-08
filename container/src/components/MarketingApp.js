577c2e6b7d520d2355aa3ee5a3ad69b8b6da08d6
nikhil sharma <ns515991@gmail.com>
2024-12-09 01:36:37 +0530

    spa routes setup

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,21 @@
-import {mount} from "marketing/MarketingApp"
+import { mount } from "marketing/MarketingApp"
 import React, { useEffect, useRef } from "react"
+import { useHistory } from "react-router-dom"
 
-
-export default function MainMarketingApp(){
+export default function MainMarketingApp() {
     const ref = useRef(null)
-    useEffect(()=>{
-        mount(ref.current)
-    },[])
-    return <div ref={ref}/>
+    const history = useHistory()
+    useEffect(() => {
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathName }) => {
+                const { pathname } = history.location
+                if (pathname !== nextPathName) {
+                    history.push(nextPathName)
+                }
+            }
+        })
+        
+        history.listen(onParentNavigate)
+    }, [])
+    return <div ref={ref} />
 }
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,20 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App"
 
-const mount = (el) =>{
+const mount = (el, { onNavigate , defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    if (onNavigate) {
+        history.listen(onNavigate)
+    }
+
     ReactDOM.render(
-        <App/>,
+        <App history={history} />,
         el
     )
-}
-
-if(process.env.NODE_ENV ==="development"){
-    const devRoot = document.querySelector("#_marketing-dev-root")
-    if(devRoot){
-        mount(devRoot)
+    return {
+        onParentNavigate: ({ pathname: nextPathName }) => {
+            
+            const { pathname } = history.location
+            if (pathname !== nextPathName) {
+                history.push(nextPathName)
+            }
+        }
     }
 }
 
-export {mount}
+if (process.env.NODE_ENV === "development") {
+    const devRoot = document.querySelector("#_marketing-dev-root")
+    if (devRoot) {
+        mount(devRoot, {
+            defaultHistory:createBrowserHistory()
+        })
+    }
+}
+
+export { mount }
